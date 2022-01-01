10feb895343ed85320af3a6e68a718567352fee0
Huaigu Lin <kevinlinhg@outlook.com>
2022-01-01 18:51:03 -0500

    show tooltip and reference line when draw live ellipses

--- src/util/drawLiveEllipse.js
+++ src/util/drawLiveEllipse.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
+import { Tooltip } from "@mui/material";
+import { pink } from "@mui/material/colors";
 import { newDraw as newDrawDispatch } from "../redux/slices/drawEllipseSlice";
 import {
   add as addDispatch,
@@ -24,7 +26,6 @@ export default function DrawLiveEllipse({ updateDrawData }) {
   // The coordinates when mouse is clicked
   const [mouseDownX, setMouseDownX] = useState();
   const [mouseDownY, setMouseDownY] = useState();
-  console.log(mouseDownX, mouseDownY);
   // The coordinates of the ellipse
   const [cx, setCx] = useState(0);
   const [cy, setCy] = useState(0);
@@ -32,9 +33,12 @@ export default function DrawLiveEllipse({ updateDrawData }) {
   const [rx, setRx] = useState(0);
   const [ry, setRy] = useState(0);
   // Flag state, true when mouse is up
-  const [mouseIsUp, setMouseIsUp] = useState(false);
+  const [mouseIsUp, setMouseIsUp] = useState(true);
   // Previous mouse is up state
   const prevMouseIsUp = usePrevious(mouseIsUp);
+  // tooltip shown when dragging and zooming
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  const [tooltipContent, setTooltipContent] = useState("");
 
   /**
    * Mouse down event handler
@@ -56,35 +60,43 @@ export default function DrawLiveEllipse({ updateDrawData }) {
     setMouseDownX((mouseDownX) => {
       setMouseDownY((mouseDownY) => {
         if (mouseDownX && mouseDownY) {
+          let rxComp;
+          let ryComp;
           if (clientX > mouseDownX && clientY > mouseDownY) {
-            const rxComp = (clientX - mouseDownX) / 2;
-            const ryComp = (clientY - mouseDownY) / 2;
+            rxComp = (clientX - mouseDownX) / 2;
+            ryComp = (clientY - mouseDownY) / 2;
             setCx(mouseDownX + rxComp);
             setCy(mouseDownY + ryComp);
             setRx(rxComp);
             setRy(ryComp);
           } else if (clientX > mouseDownX) {
-            const rxComp = (clientX - mouseDownX) / 2;
-            const ryComp = (mouseDownY - clientY) / 2;
+            rxComp = (clientX - mouseDownX) / 2;
+            ryComp = (mouseDownY - clientY) / 2;
             setCx(mouseDownX + rxComp);
             setCy(clientY + ryComp);
             setRx(rxComp);
             setRy(ryComp);
           } else if (clientY > mouseDownY) {
-            const rxComp = (mouseDownX - clientX) / 2;
-            const ryComp = (clientY - mouseDownY) / 2;
+            rxComp = (mouseDownX - clientX) / 2;
+            ryComp = (clientY - mouseDownY) / 2;
             setCx(clientX + rxComp);
             setCy(mouseDownY + ryComp);
             setRx(rxComp);
             setRy(ryComp);
           } else {
-            const rxComp = (mouseDownX - clientX) / 2;
-            const ryComp = (mouseDownY - clientY) / 2;
+            rxComp = (mouseDownX - clientX) / 2;
+            ryComp = (mouseDownY - clientY) / 2;
             setCx(clientX + rxComp);
             setCy(clientY + ryComp);
             setRx(rxComp);
             setRy(ryComp);
           }
+          setTooltipContent(
+            <span>
+              rX: {+rxComp.toFixed(2)}, rY: {+ryComp.toFixed(2)}
+            </span>
+          );
+          setTooltipOpen(true);
         }
         return mouseDownY;
       });
@@ -97,6 +109,8 @@ export default function DrawLiveEllipse({ updateDrawData }) {
    * @param {object} e mouse up event
    */
   const onMouseUp = (e) => {
+    setTooltipOpen(false);
+    setTooltipContent("");
     setMouseIsUp(true);
   };
 
@@ -159,14 +173,26 @@ export default function DrawLiveEllipse({ updateDrawData }) {
     mainMode.subMode === "ellipse" &&
     (rx || ry)
   ) {
+    console.log(rx, ry);
+    console.log(mouseIsUp);
     return (
-      <ellipse
-        cx={cx}
-        cy={cy}
-        rx={rx}
-        ry={ry}
-        style={{ fill: "#FFFFFF", stroke: "#000000" }}
-      />
+      <Fragment>
+        <Tooltip open={tooltipOpen} title={tooltipContent}>
+          <ellipse
+            cx={cx}
+            cy={cy}
+            rx={rx}
+            ry={ry}
+            style={{ fill: "#FFFFFF", stroke: "#000000" }}
+          />
+        </Tooltip>
+        {rx.toFixed() === ry.toFixed() && !mouseIsUp && (
+          <line x1={cx} y1={cy} x2={cx} y2={cy - ry} stroke={pink[400]} />
+        )}
+        {rx.toFixed() === ry.toFixed() && !mouseIsUp && (
+          <line x1={cx} y1={cy} x2={cx + rx} y2={cy} stroke={pink[400]} />
+        )}
+      </Fragment>
     );
   } else {
     return null;
